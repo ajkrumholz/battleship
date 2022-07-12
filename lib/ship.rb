cf2e57ae2e60126f994ea3fc7038cdc8b7640857
A.J. Krumholz <krumholz.alan@gmail.com>
2022-07-12 10:36:10 -0400

    Add #sunk? and test

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,9 +1,14 @@
 class Ship
-    attr_reader :name, :length
+    attr_reader :name, :length, :health
 
     def initialize(name, length)
         @name = name
         @length = length
+        @health = length
+        @sunk = false
     end
     
+    def sunk?
+        @sunk
+    end
 end
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -6,12 +6,21 @@ RSpec.describe Ship do
 
     it '1. exists' do
 
-        expect(ship).to be_instance_of(described_class)
+      expect(ship).to be_instance_of(described_class)
     end
 
     it '2. has a name and length' do
 
-        expect(ship.name).to eq("Cruiser")
-        expect(ship.length).to eq(3)
+      expect(ship.name).to eq("Cruiser")
+      expect(ship.length).to eq(3)
+    end
+
+    it '3. has health' do
+    
+      expect(ship.health).to eq(3)
+    end
+
+    it '4. is not sunk by default' do
+      expect(ship.sunk?).to eq(false)
     end
 end
