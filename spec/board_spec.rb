11b44242916587ab5131388d4d47e1c4aedf1635
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2022-07-14 08:37:18 -0600

    Comment out #valid_orientation and argument from valid_placement?, added true tests for #valid_placement?

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -27,6 +27,8 @@ RSpec.describe Board do
   it '4. validates ship placements with length' do
     expect(board.valid_placement?(cruiser, ["A1", "A2"])).to eq(false)
     expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to eq(false)
+    expect(board.valid_placement?(cruiser, ["A2", "A3", "A4"])).to eq(true)
+    expect(board.valid_placement?(submarine, ["A1", "A2"])).to eq(true)
   end
 
   it '5. validates placement coordinates are consecutive' do
