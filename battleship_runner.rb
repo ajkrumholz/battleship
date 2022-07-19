22b6a2ce719779d0cba5e35b72a5efe73853e8a3
A.J. Krumholz <krumholz.alan@gmail.com>
2022-07-19 16:11:28 -0400

    Add background music to #end_game

--- battleship_runner.rb
+++ battleship_runner.rb
@@ -1,6 +1,7 @@
 require './lib/game'
 require './lib/ship'
 require 'ruby2d'
+
 menu_theme = Sound.new('menu_theme.mp3')
 menu_theme.play
 game = Game.new
--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,7 @@
 require './lib/board'
 require './lib/player'
 require './lib/computer'
+require 'ruby2d'
 require 'pry'
 
 class Game
@@ -28,15 +29,7 @@ class Game
     print ' ' * 4 + '-' * 30 + "\n"
     print ' ' + '-' * 36
     print_very_slow(("\nWelcome to BATTLESHIP, Freedom Fighter\n"))
-    sleep 0.5
-    print ' ' + '-' * 36 + "\n"
-    print ' ' * 4 + '-' * 30 + "\n"
-    print ' ' * 7 + '-' * 24 + "\n"
-    print ' ' * 10 + '-' * 18 + "\n"
-    print ' ' * 13 + '-' * 12 + "\n"
-    print ' ' * 16 + '-' * 6 + "\n"
-    print ' ' * 18 + '-' * 2 + "\n"
-    print "\n\n"
+    sleep 1
     run_game
   end
 
@@ -47,6 +40,7 @@ class Game
     @computer.place_ships
     player_place_ships_dialog
     @player.place_ships
+    cry_havoc
     play_game
     end_game
   end
@@ -71,7 +65,7 @@ class Game
   end
 
   def difficulty_select
-    print "\n\n\nPlease select difficulty (1. Easy, 2. Hard): "
+    print "\nPlease select difficulty (1. Easy, 2. Hard): "
     answer = gets.chomp.to_i
     if answer > 0 && answer < 3
       @difficulty = answer
@@ -114,7 +108,7 @@ class Game
   end
 
   def custom_ships_menu
-    print 'Would you like to add another craft to the fleet? (y/n): '
+    print "\nWould you like to add another craft to the fleet? (y/n): "
     answer = gets.chomp
     custom_ships_menu_options(answer)
   end
@@ -160,7 +154,8 @@ class Game
   def player_place_ships_dialog
     print_very_slow("\nI have carefully placed my mighty warships on the grid." +
     "\nNow you need to lay out your, *impolite cough*, \"boats.\"\n\n")
-    sleep 0.2
+    sleep 1
+    print "\n" * 20
   end
 
   def render_boards(state = false)
@@ -169,6 +164,12 @@ class Game
     print @computer.board.render(state)
     print "=============PLAYER BOARD=============\n"
     print @player.board.render(true) + "\n"
+  end
+
+  def cry_havoc
+    print "\n" * 30
+    print_very_slow("Cry havoc, and let slip the dogs of war.", 0.07)
+    sleep 1
   end
 
   def play_game
@@ -322,6 +323,8 @@ class Game
   end
 
   def end_game
+    menu_theme = Sound.new('menu_theme.mp3')
+    menu_theme.play
     if player_wins?
       render_boards(true)
       print_very_slow("VICTORY! WE HAVE ENDED THE COLDEST WAR!!\n", 0.1)
